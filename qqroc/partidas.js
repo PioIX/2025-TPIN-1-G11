9ea4224c74a5ba9658d98cecc8250cbf87cd8829
Francisco Picca <fpicca@pioix.edu.ar>
2025-06-18 08:47:57 -0300

    Add files via upload

---
+++ qqroc/partidas.js
@@ -0,0 +1,12 @@
+let id=1
+
+class Games{
+    constructor(score, win, userId){
+        this.id=id;
+        id++
+        this.score=score;
+        this.win=win;
+        this.userId=userId;
+    }
+}
+
---
+++ qqroc/usuarios.js
@@ -0,0 +1,11 @@
+let id=1
+class Users{
+    constructor (bestGameId, name, password, id){
+        this.id = id
+        id++
+        this.bestGameId=bestGameId;
+        this.name=name;
+        this.password= password;
+        }
+
+}
